d9f83e637a976f8d4c97b06900c4214771c58509
Anandan <anandan2016@gmail.com>
2023-05-23 12:56:29 +0530

    client Lit and dashboard complited

---
+++ easybuy-customerapi/server/src/routes.ts
@@ -0,0 +1,13 @@
+//CORE_REFERENCE_IMPORTS
+//CORE_REFERENCE_IMPORT-customerList_service-SSD_SERVICE_ID_sd_74KAAt4XKsJDDJi4
+import { customerList_service as SSD_SERVICE_ID_sd_74KAAt4XKsJDDJi4 } from './sd-services/customerList/customerList_service';
+//CORE_REFERENCE_IMPORT-customerList_api-SSD_SERVICE_ID_sd_Fcx1aGnTAMxkHSbm
+import { customerList_api as SSD_SERVICE_ID_sd_Fcx1aGnTAMxkHSbm } from './sd-services/customerList/customerList_api';
+
+export const UserRoutes = [
+    //CORE_REFERENCE_PUSH_TO_ROUTE_ARRAY
+//CORE_REFERENCE_PUSH_TO_ROUTE_ARRAY-customerList_service-SSD_SERVICE_ID_sd_74KAAt4XKsJDDJi4
+SSD_SERVICE_ID_sd_74KAAt4XKsJDDJi4,
+//CORE_REFERENCE_PUSH_TO_ROUTE_ARRAY-customerList_api-SSD_SERVICE_ID_sd_Fcx1aGnTAMxkHSbm
+SSD_SERVICE_ID_sd_Fcx1aGnTAMxkHSbm,
+];
