9c552fb388337a65d8bc805b6d48985c69b0fd74
Sidharthkp <sidharthkannan20@gmail.com>
2023-05-30 10:22:37 +0530

    Trial added

--- Easybuy-ui/app/src/environments/environment.ts
+++ Easybuy-ui/app/src/environments/environment.ts
@@ -10,7 +10,7 @@ export const environment = {
         "isIDSEnabled": "false",
         "webAppMountpoint": "web",
         "NGFORAGE_MOBILE_DRIVER": "INDEXED_DB",
-        "BARCODE_READER": "DLS2eyJoYW5kc2hha2VDb2RlIjoiMTAxODQ4NDI0LVRYbFhaV0pRY205cVgyUmljZyIsIm1haW5TZXJ2ZXJVUkwiOiJodHRwczovL21sdHMuZHluYW1zb2Z0LmNvbSIsIm9yZ2FuaXphdGlvbklEIjoiMTAxODQ4NDI0IiwiY2hlY2tDb2RlIjotNDQ4Mjk4NzMzfQ==",
+        "BARCODE_READER": "DLS2eyJoYW5kc2hha2VDb2RlIjoiMTAxOTE3Nzk3LVRYbFhaV0pRY205cVgyUmljZyIsIm1haW5TZXJ2ZXJVUkwiOiJodHRwczovL21kbHMuZHluYW1zb2Z0b25saW5lLmNvbSIsIm9yZ2FuaXphdGlvbklEIjoiMTAxOTE3Nzk3IiwiY2hlY2tDb2RlIjo4ODUyMjg0NDh9",
         "backendUrl": "http://localhost:8081/api"
     }
 }
